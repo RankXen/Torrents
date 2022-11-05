83597c27be63f6888e6546936f2a0796cbb563ac
RankXen <Arno.Verkest@gmail.com>
2022-11-05 20:12:17 +0100

    Directly add torrent in flood

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
-const express = require('express');
-const TorrentSearchApi = require('torrent-search-api');
-const fs = require('fs');
-const debounce = require('debounce');
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const request = require('request');
-const copy = require('copy-to-clipboard')
+import TorrentSearchApi from "torrent-search-api";
+import fs from "fs";
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import Api from 'flood-api';
 
-app.use(express.static('public'));
+var app = express();
+const httpServer = http.createServer(app)
+const io = new Server(httpServer);
 
-app.get('/', function (req, res) {
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 TorrentSearchApi.enablePublicProviders();
 
+const settings = JSON.parse(fs.readFileSync("./settings.json", { encoding: "utf8", flag: "r" }));
+console.log(settings);
+const api = new Api({
+    baseUrl: settings.flood_uri + '/api',
+    username: settings.flood_username,
+    password: settings.flood_password,
+});
 
-io.on('connection', function (socket) {
-    socket.on('search', function (msg) {
-        console.log('Msg:', msg);
-        TorrentSearchApi.search(msg, 'All', 100).then((torrents) => {
+
+api.client.connectionTest().then((resp) => console.log(resp));
+
+io.on("connection", function (socket) {
+    socket.emit('directories', settings.directories);
+    socket.emit('flood_link', settings.flood_uri);
+
+    socket.on("search", function (msg) {
+        TorrentSearchApi.search(msg).then((torrents) => {
             socket.emit(
-                'searchResult',
+                "searchResult",
                 torrents.filter((e) => !!e.magnet)
             );
         });
     });
 
+    socket.on("add_to_flood", function (request) {
+        console.log('flood_request', request);
+        api.torrents.addUrls({
+            urls: [
+                request.magnet
+            ],
+            destination: request.destination,
+            start: true,
+          }).then((response) => {
+            console.log('flood response',response);
 
+            const success = Array.isArray(response) && response.length === 1 && response[0] === '0';
+            socket.emit("add_to_flood_response", success);
+          });
+    });
 });
 
-
-http.listen(3001, function () {
-    console.log('listening on *:3001');
+httpServer.listen(settings.port, function () {
+    console.log("listening on " +  settings.port);
 });
---
+++ readme.md
@@ -0,0 +1,25 @@
+# 1) Install dependencies
+
+
+`npm install`
+
+# 2) Fill in Settings
+
+Fill in the settings.json file:
+
+```json
+{
+    "port": 3001,
+    "flood_username": "",
+    "flood_password": "",
+    "flood_uri": "http://192.168.0.123:3000",
+    "directories": [
+        "/mnt/TVShows",
+        "/mnt/Movies"
+    ]
+}
+```
+
+# 3) Run
+
+`npm run start`
